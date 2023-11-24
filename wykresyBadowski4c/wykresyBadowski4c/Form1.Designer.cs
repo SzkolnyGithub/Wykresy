8a9de5871ab4797cf9cf9eb637ceb8b206d88d7c
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2023-11-24 19:03:20 +0100

    Skończony projekt
    Rysowanie działa git, program po prostu odmawia współpracy jeśli wprowadzisz liczbę double, also duże liczby się psują troche ale działa 5 będzie

--- wykresyBadowski4c/wykresyBadowski4c/Form1.Designer.cs
+++ wykresyBadowski4c/wykresyBadowski4c/Form1.Designer.cs
@@ -29,122 +29,123 @@ namespace wykresyBadowski4c
         /// </summary>
         private void InitializeComponent()
         {
-            this.rownanie = new System.Windows.Forms.Label();
-            this.wykres = new System.Windows.Forms.PictureBox();
-            this.a = new System.Windows.Forms.TextBox();
-            this.mZerowe = new System.Windows.Forms.Label();
-            this.b = new System.Windows.Forms.TextBox();
-            this.monotonicznosc = new System.Windows.Forms.Label();
-            this.rysuj = new System.Windows.Forms.Button();
-            this.a1 = new System.Windows.Forms.Label();
-            this.b1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.wykres)).BeginInit();
-            this.SuspendLayout();
+            rownanie = new System.Windows.Forms.Label();
+            wykres = new System.Windows.Forms.PictureBox();
+            a = new System.Windows.Forms.TextBox();
+            mZerowe = new System.Windows.Forms.Label();
+            b = new System.Windows.Forms.TextBox();
+            monotonicznosc = new System.Windows.Forms.Label();
+            rysuj = new System.Windows.Forms.Button();
+            a1 = new System.Windows.Forms.Label();
+            b1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)wykres).BeginInit();
+            SuspendLayout();
             // 
             // rownanie
             // 
-            this.rownanie.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.rownanie.Location = new System.Drawing.Point(31, 35);
-            this.rownanie.Name = "rownanie";
-            this.rownanie.Size = new System.Drawing.Size(139, 38);
-            this.rownanie.TabIndex = 0;
-            this.rownanie.Text = "y = a x + b";
+            rownanie.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            rownanie.Location = new System.Drawing.Point(31, 35);
+            rownanie.Name = "rownanie";
+            rownanie.Size = new System.Drawing.Size(139, 38);
+            rownanie.TabIndex = 0;
+            rownanie.Text = "y = a x + b";
             // 
             // wykres
             // 
-            this.wykres.BackColor = System.Drawing.SystemColors.Control;
-            this.wykres.Location = new System.Drawing.Point(372, 26);
-            this.wykres.Name = "wykres";
-            this.wykres.Size = new System.Drawing.Size(400, 400);
-            this.wykres.TabIndex = 1;
-            this.wykres.TabStop = false;
-            this.wykres.Paint += new System.Windows.Forms.PaintEventHandler(this.wykres_Paint);
-            this.wykres.MouseMove += new System.Windows.Forms.MouseEventHandler(this.wykres_MouseMove);
+            wykres.BackColor = System.Drawing.SystemColors.Control;
+            wykres.Location = new System.Drawing.Point(372, 26);
+            wykres.Name = "wykres";
+            wykres.Size = new System.Drawing.Size(400, 400);
+            wykres.TabIndex = 1;
+            wykres.TabStop = false;
+            wykres.Paint += wykres_Paint;
+            wykres.MouseMove += wykres_MouseMove;
             // 
             // a
             // 
-            this.a.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.a.Location = new System.Drawing.Point(43, 93);
-            this.a.Name = "a";
-            this.a.Size = new System.Drawing.Size(91, 29);
-            this.a.TabIndex = 3;
+            a.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            a.Location = new System.Drawing.Point(43, 93);
+            a.Name = "a";
+            a.Size = new System.Drawing.Size(91, 29);
+            a.TabIndex = 3;
             // 
             // mZerowe
             // 
-            this.mZerowe.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.mZerowe.Location = new System.Drawing.Point(12, 178);
-            this.mZerowe.Name = "mZerowe";
-            this.mZerowe.Size = new System.Drawing.Size(354, 39);
-            this.mZerowe.TabIndex = 5;
-            this.mZerowe.Text = "Miejsce zerowe: ";
+            mZerowe.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            mZerowe.Location = new System.Drawing.Point(12, 178);
+            mZerowe.Name = "mZerowe";
+            mZerowe.Size = new System.Drawing.Size(354, 39);
+            mZerowe.TabIndex = 5;
+            mZerowe.Text = "Miejsce zerowe: ";
             // 
             // b
             // 
-            this.b.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.b.Location = new System.Drawing.Point(43, 144);
-            this.b.Name = "b";
-            this.b.Size = new System.Drawing.Size(91, 29);
-            this.b.TabIndex = 6;
+            b.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            b.Location = new System.Drawing.Point(43, 144);
+            b.Name = "b";
+            b.Size = new System.Drawing.Size(91, 29);
+            b.TabIndex = 6;
             // 
             // monotonicznosc
             // 
-            this.monotonicznosc.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.monotonicznosc.Location = new System.Drawing.Point(12, 315);
-            this.monotonicznosc.Name = "monotonicznosc";
-            this.monotonicznosc.Size = new System.Drawing.Size(354, 39);
-            this.monotonicznosc.TabIndex = 7;
-            this.monotonicznosc.Text = "Monotoniczność: ";
+            monotonicznosc.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            monotonicznosc.Location = new System.Drawing.Point(12, 315);
+            monotonicznosc.Name = "monotonicznosc";
+            monotonicznosc.Size = new System.Drawing.Size(354, 39);
+            monotonicznosc.TabIndex = 7;
+            monotonicznosc.Text = "Monotoniczność: ";
             // 
             // rysuj
             // 
-            this.rysuj.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.rysuj.Location = new System.Drawing.Point(12, 392);
-            this.rysuj.Name = "rysuj";
-            this.rysuj.Size = new System.Drawing.Size(106, 34);
-            this.rysuj.TabIndex = 8;
-            this.rysuj.Text = "Rysuj wykres";
-            this.rysuj.UseVisualStyleBackColor = true;
-            this.rysuj.Click += new System.EventHandler(this.rysuj_Click);
+            rysuj.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            rysuj.Location = new System.Drawing.Point(12, 392);
+            rysuj.Name = "rysuj";
+            rysuj.Size = new System.Drawing.Size(106, 34);
+            rysuj.TabIndex = 8;
+            rysuj.Text = "Rysuj wykres";
+            rysuj.UseVisualStyleBackColor = true;
+            rysuj.Click += rysuj_Click;
             // 
             // a1
             // 
-            this.a1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.a1.Location = new System.Drawing.Point(12, 96);
-            this.a1.Name = "a1";
-            this.a1.Size = new System.Drawing.Size(25, 23);
-            this.a1.TabIndex = 9;
-            this.a1.Text = "a";
+            a1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            a1.Location = new System.Drawing.Point(3, 96);
+            a1.Name = "a1";
+            a1.Size = new System.Drawing.Size(34, 23);
+            a1.TabIndex = 9;
+            a1.Text = "a = ";
             // 
             // b1
             // 
-            this.b1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.b1.Location = new System.Drawing.Point(12, 144);
-            this.b1.Name = "b1";
-            this.b1.Size = new System.Drawing.Size(25, 23);
-            this.b1.TabIndex = 11;
-            this.b1.Text = "b";
+            b1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            b1.Location = new System.Drawing.Point(3, 144);
+            b1.Name = "b1";
+            b1.Size = new System.Drawing.Size(34, 23);
+            b1.TabIndex = 11;
+            b1.Text = "b =";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.b1);
-            this.Controls.Add(this.a1);
-            this.Controls.Add(this.rysuj);
-            this.Controls.Add(this.monotonicznosc);
-            this.Controls.Add(this.b);
-            this.Controls.Add(this.mZerowe);
-            this.Controls.Add(this.a);
-            this.Controls.Add(this.wykres);
-            this.Controls.Add(this.rownanie);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.wykres)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(b1);
+            Controls.Add(a1);
+            Controls.Add(rysuj);
+            Controls.Add(monotonicznosc);
+            Controls.Add(b);
+            Controls.Add(mZerowe);
+            Controls.Add(a);
+            Controls.Add(wykres);
+            Controls.Add(rownanie);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Wykresy";
+            Load += Form1_Load;
+            MouseMove += Form1_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)wykres).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- wykresyBadowski4c/wykresyBadowski4c/Form1.cs
+++ wykresyBadowski4c/wykresyBadowski4c/Form1.cs
@@ -12,91 +12,113 @@ namespace wykresyBadowski4c
 {
     public partial class Form1 : Form
     {
-        int A, B = 0;
+        int A = 0;
+        int B = 0;
+        bool test = false;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void wykres_Paint(object sender, PaintEventArgs e)
         {
             Graphics gdi = e.Graphics;
             Pen p = new Pen(Color.Black, 2);
-                Pen pNew = new Pen(Color.Gray, 1);
-                Point p1 = new Point(0, 200);
-                Point p2 = new Point(400, 200);
-                Point p3 = new Point(200, 0);
-                Point p4 = new Point(200, 400);
-                gdi.DrawLine(p, p1, p2);
-                gdi.DrawLine(p, p3, p4);
-                int i = 0;
-                while (i <= 400)
+            Pen pNew = new Pen(Color.Gray, 1);
+            Point p1 = new Point(0, 200);
+            Point p2 = new Point(400, 200);
+            Point p3 = new Point(200, 0);
+            Point p4 = new Point(200, 400);
+            gdi.DrawLine(p, p1, p2);
+            gdi.DrawLine(p, p3, p4);
+            int i = 0;
+            while (i <= 400)
+            {
+                gdi.DrawLine(pNew, i - 1, -1, i - 1, 399);
+                gdi.DrawLine(pNew, 0, i, 400, i);
+                if (i == 0)
                 {
-                    gdi.DrawLine(pNew, i - 1, -1, i - 1, 399);
-                    gdi.DrawLine(pNew, 0, i, 400, i);
-                    if (i == 0)
-                    {
-                        gdi.DrawLine(p, i + 1, 190, i + 1, 210);
-                        gdi.DrawLine(p, 190, i + 1, 210, i + 1);
-                        gdi.DrawLine(pNew, i, 0, i, 400);
-                        gdi.DrawLine(p, 200, 0, 200, 0);
-                    }
-                    else if (i > 0 && i < 400)
-                    {
-                        gdi.DrawLine(p, i, 190, i, 210);
-                        gdi.DrawLine(p, 190, i, 210, i);
-                    }
-                    else if (i == 400)
-                    {
-                        gdi.DrawLine(p, i + 1, 191, i + 1, 211);
-                        gdi.DrawLine(pNew, -1, i - 1, 399, i - 1);
-                        gdi.DrawLine(p, 189, i - 1, 209, i - 1);
-                    }
-                    i += 40;
-                    gdi.DrawLine(p, 390, 185, 400, 200);
-                    gdi.DrawLine(p, 390, 215, 400, 200);
-                    gdi.DrawLine(p, 215, 10, 200, 0);
-                    gdi.DrawLine(p, 185, 10, 200, 0);
+                    gdi.DrawLine(p, i + 1, 190, i + 1, 210);
+                    gdi.DrawLine(p, 190, i + 1, 210, i + 1);
+                    gdi.DrawLine(pNew, i, 0, i, 400);
+                    gdi.DrawLine(p, 200, 0, 200, 0);
                 }
-            int y0 = B;
-            int y11 = A * 11 + B;
-            Point start = new Point(0, y0);
-            Point koniec = new Point(400, y11);
-            e.Graphics.DrawLine(new Pen(Color.Red, 2), start, koniec);
-
+                else if (i > 0 && i < 400)
+                {
+                    gdi.DrawLine(p, i, 190, i, 210);
+                    gdi.DrawLine(p, 190, i, 210, i);
+                }
+                else if (i == 400)
+                {
+                    gdi.DrawLine(p, i + 1, 191, i + 1, 211);
+                    gdi.DrawLine(pNew, -1, i - 1, 399, i - 1);
+                    gdi.DrawLine(p, 189, i - 1, 209, i - 1);
+                }
+                i += 40;
+                gdi.DrawLine(p, 390, 185, 400, 200);
+                gdi.DrawLine(p, 390, 215, 400, 200);
+                gdi.DrawLine(p, 215, 10, 200, 0);
+                gdi.DrawLine(p, 185, 10, 200, 0);
+            }
+            if(test)
+            {
+                int xM5 = -5; // wspolrzedna x dla punktu x = -5 to 0, nwm co trzeba zrobic z y jeszcze
+                int x5 = 5;// dla 5 jest to 400, jeden "krok" jest co 40 px
+                int yxM5 = (A * xM5 + B) * 40; // dla xM5 = -5, A = 2 i B = 4, yxM5 = -6, czyli * 40 = -240 - wspolrzedna y to 440
+                int yx5 = (A * x5 + B) * 40; // dla x5 = 5, A = 2 i B = 4, yx5 = 14, czyli * 40 = 560 - wspolrzedna y to -360
+                yxM5 = yxM5 * (-1) + 200;
+                yx5 = yx5 * (-1) + 200;
+                Point start = new Point(0, yxM5);
+                Point end = new Point(400, yx5);
+                gdi.DrawLine(new Pen(Color.Red, 2), start, end);
+                //mZerowe.Text = yx5 + " + " + yxM5;
+            }
         }
         // https://itecnote.com/tecnote/c-button-click-eventargs-e-information/
         private void wykres_MouseMove(object sender, MouseEventArgs e)
         {
-            mZerowe.Text = "X: " + e.X + " Y: " + e.Y;
+            //mZerowe.Text = "X: " + e.X + " Y: " + e.Y;
         }
 
         private void rysuj_Click(object sender, EventArgs e)
         {
-            double A = Convert.ToInt32(a.Text);
-            double B = Convert.ToInt32(b.Text);
-                if (A > 0)
-                {
-                    monotonicznosc.Text = "Monotoniczność: funkcja rosnąca";
-                }
-                else if (A < 0)
-                {
-                    monotonicznosc.Text = "Monotoniczność: funkcja malejąca";
-                }
-                else
-                {
-                    monotonicznosc.Text = "Monotoniczność: funkcja stała";
-                }
-                double miejsce0 = (-B / A);
+            if (a.Text == null || a.Text == "" || b.Text == null || b.Text == "")
+            {
+                return;
+            }
+            A = Convert.ToInt32(a.Text);
+            B = Convert.ToInt32(b.Text);
+            double aa = Convert.ToInt32(a.Text);
+            double bb = Convert.ToInt32(b.Text);
+            if (A > 0)
+            {
+                monotonicznosc.Text = "Monotoniczność: funkcja rosnąca";
+            }
+            else if (A < 0)
+            {
+                monotonicznosc.Text = "Monotoniczność: funkcja malejąca";
+            }
+            else
+            {
+                monotonicznosc.Text = "Monotoniczność: funkcja stała";
+            }
+            if(A == 0)
+            {
+                string miejsce0 = "Brak";
                 mZerowe.Text = "Miejsce zerowe: " + miejsce0;
+            } else
+            {
+                double miejsce0 = (-bb / aa);
+                mZerowe.Text = "Miejsce zerowe: " + miejsce0;
+            }
             //-----------------------------------------------------------------------------//
-            wykres.Invalidate();
-            wykres.Update();
+            test = true;
+            wykres.Refresh();
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //mZerowe.Text = "X: " + e.X + " Y: " + e.Y;
         }
     }
 }
